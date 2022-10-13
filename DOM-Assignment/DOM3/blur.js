b46d64ba3a8b086aaeb741eb8cb19fc088dce4da
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-10-13 10:55:07 +0530

    okie

---
+++ DOM-Assignment/DOM3/blur.js
@@ -0,0 +1,7 @@
+function getData() {
+    console.log(document.getElementById('uname').value)
+    /*   let uname = document.getElementById('uname').value
+      /*  alert(uname)
+       console.log("Test Case 123") */
+    //document.getElementById('uname').value = uname.toUpperCase() * /
+}
