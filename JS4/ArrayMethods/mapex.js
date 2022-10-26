34cb86c5b747fe4d2dd193126380afb023dac24e
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-10-26 10:40:57 +0530

    okie

---
+++ JS4/ArrayMethods/mapex.js
@@ -0,0 +1,4 @@
+let ids = [10, 20, 30, 40]
+ids.map((id, index, arr) => {
+    console.log(id, ":", index, ":", arr)
+})
---
+++ JS4/treepledot/one.js
@@ -0,0 +1,7 @@
+let marks = [35, 38, 39, 40, 99]
+//create new array based on exist array
+let new_Marks = [...marks]
+/* let new_Marks = marks.map((mark) => {
+    return mark
+}) */
+console.log(new_Marks)
